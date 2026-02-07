6d04eed3004eb8aeb1bedff3da59366762ea1ff0
EricDark231 <128099775+EricDark231@users.noreply.github.com>
2026-02-07 09:50:34 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -167,8 +167,7 @@ if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
 log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[rvxgale](https://github.com/EricDark231/RVXGALE)\n"
-log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
+log "\n[RVX GALE & MORPHE](https://github.com/EricDark231/RVXGALE)\n"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
 if [ -n "$SKIPPED" ]; then
